55a09cc598b1203b7ac0ab45a716cf2ec45abf79
Lee <359268127@qq.com>
2016-08-15 16:18:30 +0800

    添加基础控制器

--- LeeLin.Web/Controllers/HomeController.cs
+++ LeeLin.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace LeeLin.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
